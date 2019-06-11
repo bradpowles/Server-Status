c4bb0b7e2601f74f13623355bf60b5cf6480869e
Brad Powles <pwizz@hotmail.co.uk>
2019-06-11 17:37:03 +0100

    Base config values.

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ class Config(object):
     DEBUG = True
     DEVELOPMENT = True
     SECRET_KEY = 'guess-me-if-you-can'
-    DB_HOST = 'mongodb://192.168.10.130:27017'
-    DB_COLLECTION = 'presstest'
+    DB_HOST = 'mongodb://127.0.0.1:27017'
+    DB_COLLECTION = 'server-status'
